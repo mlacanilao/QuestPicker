58a236ae11abf1768b4192b267ca1a39dc03a175
Michael Lacanilao <michael.v.lacanilao@gmail.com>
2024-12-21 11:51:36 -0500

    Add check for ModOptions assembly to prevent reflection issues

--- QuestPicker/QuestPicker.cs
+++ QuestPicker/QuestPicker.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System;
+using System.Linq;
+using BepInEx;
 using HarmonyLib;
 using QuestPicker.Config;
 using UnityEngine;
@@ -11,28 +13,56 @@ namespace QuestPicker
         internal const string Name = "Quest Picker";
         internal const string Version = "1.0.0.0";
         internal const string ModOptionsGuid = "evilmask.elinplugins.modoptions";
+        internal const string ModOptionsAssemblyName = "ModOptions";
     }
 
     [BepInPlugin(GUID: ModInfo.Guid, Name: ModInfo.Name, Version: ModInfo.Version)]
     internal class QuestPicker : BaseUnityPlugin
     {
+        internal static QuestPicker Instance { get; private set; }
+        
         private void Start()
         {
+            Instance = this;
+            
             QuestPickerConfig.LoadConfig(config: Config);
 
             Harmony.CreateAndPatchAll(type: typeof(Patcher));
             
+            if (IsModOptionsInstalled())
+            {
+                try
+                {
+                    UI.UIController.RegisterUI();
+                }
+                catch (Exception ex)
+                {
+                    Log(payload: $"An error occurred during UI registration: {ex.Message}");
+                }
+            }
+            else
+            {
+                Log(payload: "Mod Options is not installed. Skipping UI registration.");
+            }
+        }
+        
+        private static void Log(object payload)
+        {
+            Instance.Logger.LogInfo(data: payload);
+        }
+        
+        private bool IsModOptionsInstalled()
+        {
             try
             {
-                UI.UIController.RegisterUI();
+                return AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .Any(predicate: assembly => assembly.GetName().Name == ModInfo.ModOptionsAssemblyName);
             }
-            catch (System.IO.FileNotFoundException)
+            catch (Exception ex)
             {
-                Debug.LogWarning("[Quest Picker] Mod Options mod is not installed/enabled. Skipping UI registration.");
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"[Quest Picker] An unexpected error occurred: {ex.Message}");
+                Log(payload: $"Error while checking for Mod Options: {ex.Message}");
+                return false;
             }
         }
     }
